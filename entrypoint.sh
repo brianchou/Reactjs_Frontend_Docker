8fe924d5190ec4a35849a274a7dd7c52a00a12b8
Brian Chou <brianchou_5@hotmail.com>
2021-04-10 14:52:35 +0800

    test CI

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# `$*` expands the `args` supplied in an `array` individually
+# or splits `args` in a string separated by whitespace.
+
+sh -c "apt-get install npm"
+
+sh -c "cd /"
+
+sh -c "npx create-react-app reactjs-frontend-docker"
+
+sh -c "cd /reactjs-frontend-docker"
+
+sh -c "npm install bootstrap"
+
+sh -c "npm install react-router-dom"
+
+sh -c "npm install axios"
