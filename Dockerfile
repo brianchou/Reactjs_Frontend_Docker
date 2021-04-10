d3dac635749606d438ab85d4c8b33d597b8a6316
Brian Chou <brianchou_5@hotmail.com>
2021-04-10 17:37:03 +0800

    test CI

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-# Container image that runs your code
 FROM ubuntu:20.04
-MAINTAINER Brian Chou
 
-RUN npm start
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Executes `entrypoint.sh` when the Docker container starts up
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,4 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 
-sh -c "su"
-
-sh -c "apt-get update"
-
-sh -c "apt-get install npm"
-
-sh -c "cd /"
-
-sh -c "npx create-react-app reactjs-frontend-docker"
-
-sh -c "cd /reactjs-frontend-docker"
-
-sh -c "npm install bootstrap"
-
-sh -c "npm install react-router-dom"
-
-sh -c "npm install axios"
+sh -c "npm start"
