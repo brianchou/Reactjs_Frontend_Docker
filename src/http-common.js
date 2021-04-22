a878872ae856719838e5341e9de7370fe960c291
Brian Chou <brianchou_5@hotmail.com>
2021-04-22 09:36:54 +0800

    Update baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://nodejs-express-backend-heroku.herokuapp.com/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
